897b063570481ea908df9d88ddd46b628dd87578
Enjoy-Maity <64701616+Enjoy-Maity@users.noreply.github.com>
2023-06-26 16:55:48 +0530

    added new conditions to check for duplicacy

--- Cli_preparation_task.py
+++ Cli_preparation_task.py
@@ -1,13 +1,13 @@
-import pandas as pd
-import traceback
-import pythoncom
-from openpyxl import load_workbook
-from tkinter import messagebox
-import win32com.client as win32
-from CustomException import CustomException
-from Custom_Thread import CustomThread
-from pathlib import Path
-# import os
+import pandas as pd							# for dataframe manipulation
+import traceback							# for error traceback to line number and statement causing error
+import pythoncom							# to handle windows processes
+from openpyxl import load_workbook,Workbook	# to handle the excel files.
+from tkinter import messagebox				# to create message dialogues
+import win32com.client as win32				# for MS Office application handling
+from CustomException import CustomException	# Custom class for handling custom raised exceptions
+from Custom_Thread import CustomThread		# Custom class Threads with return value
+from pathlib import Path					# for handling paths of files and folders
+import os
 # import numpy as np
 # import re
 
@@ -41,13 +41,30 @@ def section_splitter(dataframe):
 				if(((j+2) < len(dataframe)) and (dataframe.iloc[j+2][0].__contains__(string_to_be_found))):
 					break
 				else:
+					
 					for k in range(len(columns)):
 						dictionary_for_columns[columns[k]].append(dataframe.iloc[j][k])
 					j+=1
 			i = j+2
 			
 			section_dictionary[section] = pd.DataFrame(dictionary_for_columns,columns=columns)
-			section_dictionary[section].drop_duplicates(keep = False,inplace = True)
+			copy_df = section_dictionary[section].copy()
+			section_dictionary[section].drop_duplicates(keep = "first", inplace = True)
+			if(section.strip().upper() == "VPLS"):
+				if(len(copy_df)> len(section_dictionary[section])):
+					duplicate_vpls_id_list = []
+					duplicate_value_counts = copy_df.duplicated(keep='first').tolist()
+					# print(duplicate_value_counts)
+					# print({i:j for i,j in enumerate(duplicate_value_counts)})
+					# print(copy_df['VPLS ID'].value_counts())
+					
+					
+					for indx in range(0,len(duplicate_value_counts)):
+						if(duplicate_value_counts[indx]):
+							duplicate_vpls_id_list.append(copy_df.iloc[indx]['VPLS ID'])
+
+					messagebox.showinfo("Duplicate VPLS ID Rows found",f"Duplicate rows found for below vpls ids:\n{', '.join(str(i) for i in duplicate_vpls_id_list)}")
+
 			section_dictionary[section].replace("TempNA","",inplace = True)
 			
 		else:
@@ -70,7 +87,10 @@ def main(workbook):
 				nodes.append(i)
 		
 		if(len(nodes) == 0):
-			raise CustomException('	Node details absent!',f"The workbook {workbook1} doesn't contain the Node details")
+			raise CustomException('	Node details absent!',f"The workbook {workbook1} doesn't contain the Node details, Please Check!")
+		
+		if(len(nodes) < len(sheetnames)):
+			raise CustomException('	Node details incorrect!',f"The workbook {workbook1} contain incorrect Node details, Please Check!")
 		
 		else:
 			cli = ""
@@ -90,30 +110,54 @@ def main(workbook):
 				dictionary_for_node_section[sheetname] = section_splitter(df)
 		
 			keys = dictionary_for_node_section.keys()
-			
-			cli = "configure service\n"
+			temp_flag = "Unsuccessful"
+
 			for key in keys:
+				cli = ""
 				secondary_keys = dictionary_for_node_section[key].keys()
 				for second_key in secondary_keys:
 					if((second_key.upper() == "VPLS") and (len(dictionary_for_node_section[key][second_key]) > 0)):
+
 						unique_vpls_id_len = len(dictionary_for_node_section[key][second_key]['VPLS ID'].unique())
 						dataframe_len = len(dictionary_for_node_section[key][second_key])
 
 						if(unique_vpls_id_len < dataframe_len):
-							raise CustomException("	Duplicate VPLS ID found!","Kindly Check the 'VPLS' Section for duplicate 'VPLS ID' column!")
+							duplicate_vpls_id_list = []
+							duplicate_value_counts = dictionary_for_node_section[key][second_key]['VPLS ID'].value_counts().index.tolist()
+							
+							for vpls_id in duplicate_value_counts:
+								if(int(dictionary_for_node_section[key][second_key]['VPLS ID'].value_counts()[vpls_id]) > 1):
+									duplicate_vpls_id_list.append(vpls_id)
+							
+							raise CustomException("	Duplicate VPLS ID found!",f"Kindly Check the 'VPLS' Section for below duplicate 'VPLS ID':\n{', '.join(str(vpls_id_num) for vpls_id_num in duplicate_vpls_id_list)}")
 						
 						else:
 							import VPLS_CLI_prepare
+							# print(key)
+							cli = f"{cli}configure service\n"
 							thread = CustomThread(target = VPLS_CLI_prepare.cli_maker, args=(dictionary_for_node_section[key][second_key],key,cli))
 							thread.daemon = True
 							thread.start()
 							cli = thread.join()
+
+							if(cli.strip() == "Unsuccessful"):
+								temp_flag = "Unsuccessful"
+								raise CustomException("	Exception Occured!",f"Cli Creation is Unsuccessful")
+							
+							
+							cli = f"{cli}exit all\n"
 			
 				cli = f"{cli}\
-					exit all\n\
 						admin save"
 				
-				if(cli != "Unsuccesful"):
+				if(temp_flag != "Unsuccesful"):
+					# if(Path(file_to_write).exists()):
+					# 	os.remove(file_to_write)
+					# 	wb = Workbook()
+					# 	wb.save(file_to_write)
+					# 	wb.close()
+					# 	del wb
+
 					import cli_dumper
 					cli_dumper.cli_writer(cli,file_to_write,key)
 
@@ -121,6 +165,7 @@ def main(workbook):
 				workbook2 = file_to_write
 				wb = load_workbook(file_to_write)
 				sheetnames = wb.sheetnames
+				# print(sheetnames)
 
 				if(len(sheetnames) > 1):
 					for sheetname in sheetnames:
@@ -132,6 +177,7 @@ def main(workbook):
 
 			flag = "Successful"
 			messagebox.showinfo("	CLI Created Successfully!",f"CLI created at {file_to_write} successfully!")
+	
 	except CustomException:
 		flag = "Unsuccessful"
 	
@@ -149,16 +195,16 @@ def main(workbook):
 	finally:
 		# global workbook1;
 
-		excel = win32.Dispatch("Excel.Application")
+		# excel = win32.Dispatch("Excel.Application")
 
-		if((len(workbook1) > 0) and (Path(workbook1).exists())):
-			wb = excel.Workbooks.Open(workbook1)
-			wb.Close()
+		# if((len(workbook1) > 0) and (Path(workbook1).exists())):
+		# 	wb = excel.Workbooks.Open(workbook1)
+		# 	wb.Close()
 		
-		if((len(workbook2) > 0) and (Path(workbook2).exists())):
-			wb = excel.Workbooks.Open(workbook2)
-			wb.Close()
-		excel.Application.Quit()
+		# if((len(workbook2) > 0) and (Path(workbook2).exists())):
+		# 	wb = excel.Workbooks.Open(workbook2)
+		# 	wb.Close()
+		# excel.Application.Quit()
 		return flag
         
         
