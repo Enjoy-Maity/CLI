48a13f408f6998390c96bb920d609ce356acc961
Enjoy-Maity <64701616+Enjoy-Maity@users.noreply.github.com>
2023-10-17 17:30:58 +0530

    Created class for Custom Thread

---
+++ Main-application/CustomThread.py
@@ -0,0 +1,19 @@
+# Importing Thread Class
+from threading import Thread
+
+# Defining CustomThread, because the original class doesn't return anything.
+class CustomThread(Thread):
+    def __init__(self,
+                 group=None, target=None, name=None, 
+                 args=(),kwargs={},Verbose=None):
+        
+        Thread.__init__(self,group,target,name,args,kwargs)
+        self._returnvalue = None                            # variable returning value after executing
+    
+    def run(self):
+        if(self._target is not None):
+            self._returnvalue = self._target(*self._args,**self._kwargs)
+
+    def join(self):
+        Thread.join(self)
+        return self._returnvalue
