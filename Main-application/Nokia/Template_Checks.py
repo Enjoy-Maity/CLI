a21248666d8dcd8d6c64dacc08192ef89c100a9c
Enjoy-Maity <64701616+Enjoy-Maity@users.noreply.github.com>
2023-10-17 19:57:18 +0530

    added logging

--- Main-application/Nokia/Template_Checks.py
+++ Main-application/Nokia/Template_Checks.py
@@ -1,5 +1,8 @@
+from pathlib import Path
 import sys
 import os
+import logging
+import inspect
 # Getting the parent directory of the folder
 # parent_directory = str(Path(__file__).resolve().parents[1])
 #or
@@ -10,8 +13,22 @@ sys.path.append(parent_directory)   #Adding the parent in system path
 
 import Custom_Exception as ce 
 
-try:
-    raise ce.CustomException("Test Title","Test message")
+log_file_path = "C:/Ericsson_Application_Logs/CLI_Automation_Logs/"
+Path(log_file_path).mkdir(parents=True,exist_ok=True)
 
-except ce.CustomException:
-    pass
+log_file = os.path.join(log_file_path,"Template_checks.log")
+
+logging.basicConfig(filename=log_file,
+                             filemode="a",
+                             format=f"[ {'%(asctime)s'} ]: <<{'%(levelname)s'}>>: ({os.path.basename(os.path.dirname(__file__))}/{'%(module)s'}): {'%(message)s'}",
+                             datefmt='%d-%b-%Y %I:%M:%S %p',
+                             encoding= "UTF-8",
+                             level=logging.DEBUG)
+
+
+def main_func(**kwargs):
+    file_name = kwargs['filename']
+    logging.info("Created and tested logging")
+    
+
+main_func(filename="Test File")
