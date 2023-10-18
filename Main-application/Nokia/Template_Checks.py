6e7d928ef018f43a25556ecc480d99d6244241a3
Enjoy-Maity <64701616+Enjoy-Maity@users.noreply.github.com>
2023-10-18 21:10:16 +0530

    tested some things

--- Main-application/Nokia/Template_Checks.py
+++ Main-application/Nokia/Template_Checks.py
@@ -2,7 +2,6 @@ from pathlib import Path
 import sys
 import os
 import logging
-import inspect
 # Getting the parent directory of the folder
 # parent_directory = str(Path(__file__).resolve().parents[1])
 #or
@@ -19,16 +18,16 @@ Path(log_file_path).mkdir(parents=True,exist_ok=True)
 log_file = os.path.join(log_file_path,"Template_checks.log")
 
 logging.basicConfig(filename=log_file,
-                             filemode="a",
-                             format=f"[ {'%(asctime)s'} ]: <<{'%(levelname)s'}>>: ({os.path.basename(os.path.dirname(__file__))}/{'%(module)s'}): {'%(message)s'}",
-                             datefmt='%d-%b-%Y %I:%M:%S %p',
-                             encoding= "UTF-8",
-                             level=logging.DEBUG)
+                    filemode="a",
+                    format=f"[ {'%(asctime)s'} ]: <<{'%(levelname)s'}>>: ({os.path.basename(os.path.dirname(__file__))}/{'%(module)s'}): {'%(message)s'}",
+                    datefmt='%d-%b-%Y %I:%M:%S %p',
+                    encoding= "UTF-8",
+                    level=logging.DEBUG)
 
 
 def main_func(**kwargs):
     file_name = kwargs['filename']
-    logging.info("Created and tested logging")
+    
     
 
 main_func(filename="Test File")
