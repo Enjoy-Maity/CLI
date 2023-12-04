13a10df36bb06e0f3edecb872939387b38bd5b5c
Enjoy-Maity <enjoymaity@gmail.com>
2023-12-04 20:43:30 +0530

    Created the module for Nokia Running Config Checks

---
+++ Main-application/Nokia/Nokia_Running_Config_Checks.py
@@ -0,0 +1,12 @@
+import logging
+
+def main_func(**kwargs):
+    """
+        Performs the general node checks and calls the section specific node checks modules
+        
+        Arguments : (**kwargs)
+            **kwargs ====> 
+            
+        returns flag
+    """
+    pass
