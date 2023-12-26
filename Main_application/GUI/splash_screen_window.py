4e164037cf41038d520dbd88d42c7b2e8352d138
Enjoy-Maity <enjoymaity@gmail.com>
2023-12-26 09:47:28 +0530

    changed the progress bar radius

---
+++ Main_application/GUI/splash_screen_window.py
@@ -0,0 +1,44 @@
+import sys
+from ui_splash_screen import Ui_splash_screen
+from PySide6 import QtCore, QtGui, QtWidgets
+from PySide6.QtCore import Qt,QTimer
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont, QFontDatabase, QIcon, QKeySequence, QLinearGradient, QPalette, QPainter, QPixmap, QRadialGradient)
+from PySide6.QtWidgets import QMainWindow, QApplication
+
+class Splash_screen(QMainWindow):
+    def __init__(self,main_window):
+        QMainWindow.__init__(self)
+        self.main_window = main_window
+        self.ui = Ui_splash_screen()
+        self.ui.setupUi(self)
+        
+        ## Removing the Title Bar
+        self.setWindowFlag(Qt.FramelessWindowHint)
+        
+        
+        ## Qtimer ===> start
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.progress)
+        
+        # Timer in milliseconds
+        self.timer.start(35)
+        self.counter = 0
+    
+    def progress(self):
+        
+        self.ui.progressBar.setValue(self.counter)
+        
+        # Set Value to Progress Bar
+        if(self.counter > 100):
+            # Stop Timer
+            self.timer.stop()
+            
+            # Show main Window
+        
+        self.counter+=1
+            
+app = QApplication(sys.argv)
+main_window = QMainWindow()
+window = Splash_screen(main_window)
+window.show()
+sys.exit(app.exec())
